95be8cca6eb178d89ba8007cdfd9a5d7203f1f39
Valentin Macheret <vmacheret@scaleway.com>
2024-07-05 15:17:12 +0200

    feat(ci): clear setup

--- cirq_scaleway/versions.py
+++ cirq_scaleway/versions.py
@@ -16,7 +16,7 @@ import platform
 
 from typing import Final
 
-CIRQ_VERSION: Final = importlib.metadata.version("cirq")
+CIRQ_VERSION: Final = importlib.metadata.version("cirq-core")
 CIRQ_SCALEWAY_PROVIDER_VERSION: Final = importlib.metadata.version("cirq-scaleway")
 
 __version__: Final = CIRQ_SCALEWAY_PROVIDER_VERSION
@@ -25,6 +25,6 @@ USER_AGENT: Final = " ".join(
     [
         f"cirq-scaleway/{CIRQ_SCALEWAY_PROVIDER_VERSION}",
         f"({platform.system()}; {platform.python_implementation()}/{platform.python_version()})",
-        f"cirq/{CIRQ_VERSION}",
+        f"cirq-core/{CIRQ_VERSION}",
     ]
 )
