e7d8c7e1b543f6c82f94a195e50932188698b521
Valentin Macheret <vmacheret@scaleway.com>
2025-07-29 19:01:18 +0200

    fix(ver): update python version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [r.strip() for r in requirements]
 
 setup(
     name="cirq_scaleway",
-    version="0.1.4",
+    version="0.1.5",
     project_urls={
         "Documentation": "https://www.scaleway.com/en/quantum-as-a-service/",
         "Source": "https://github.com/scaleway/cirq-scaleway",
@@ -32,7 +32,7 @@ setup(
     author_email="vmacheret@scaleway.com",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires=(">=3.10.0"),
+    python_requires=(">=3.12.0"),
     license="Apache 2",
     description=description,
     long_description=long_description,
