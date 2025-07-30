ee0bb5480a5d81fcc03e4deeab91b6a2380477f1
Valentin Macheret <vmacheret@scaleway.com>
2025-07-30 10:32:11 +0200

    fix(ver): try fix req

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ description = "A Cirq package to connect to Scaleway Quantum as a Service"
 long_description = io.open("README.rst", encoding="utf-8").read()
 requirements = open("requirements.txt").readlines()
 requirements = [r.strip() for r in requirements]
-requirements += [f"cirq-core==1.3.0"]
 
 setup(
     name="cirq_scaleway",
@@ -33,7 +32,7 @@ setup(
     author_email="vmacheret@scaleway.com",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires=(">=3.12.0"),
+    python_requires=(">=3.13.0"),
     license="Apache 2",
     description=description,
     long_description=long_description,
